38dc2cc95f8a5370299defa47688937285c8bb01
Shiminmandy <mandyshimin18@gmail.com>
2020-08-26 20:29:42 -0400

    Cat1

--- src/com/imooc/animal/Cat1.java
+++ src/com/imooc/animal/Cat1.java
@@ -1,4 +1,14 @@
 package com.imooc.animal;
 
 public class Cat1 {
+    //成员属性：昵称
+    String name;//昵称
+
+    public void run(Cat one){
+        System.out.println("昵称为："+one.name+"的小猫在奔跑");
+    }
+
+    public void call(){
+        run(this);//谁调用了call,谁就是this
+    }
 }
