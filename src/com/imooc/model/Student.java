cd5c0defc6271e53ab4a4f1a7f5b456314a743d2
Shiminmandy <mandyshimin18@gmail.com>
2020-08-31 19:29:09 -0400

    ComprehenesiveCase2

---
+++ src/com/imooc/model/Student.java
@@ -0,0 +1,75 @@
+package com.imooc.model;
+
+public class Student {
+    //成员属性：学号、姓名、性别、年龄
+    private String studentNo;
+    private String studentName;
+    private String studentGender;
+    private int studentAge;
+
+    //无参构造
+    public Student(){
+
+    }
+
+    //多参构造，实现对全部属性的赋值
+    public Student(String studentNo,String studentName,String studentGender,int studentAge){
+        this.setStudentNo(studentNo);
+        this.setStudentName(studentName);
+        this.setStudentGender(studentGender);
+        this.setStudentAge(studentAge);
+    }
+
+    public String getStudentNo() {
+        return this.studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentGender() {
+        return this.studentGender;
+    }
+
+    public void setStudentGender(String studentGender) {
+        //限制性别只能是男或女,反之，赋值为男
+        if(studentGender=="男"||studentGender=="女")
+            this.studentGender = studentGender;
+        else
+            this.studentGender="男";
+    }
+
+    /**
+     * 给年龄赋值，限定必须在10--100之间，反之赋值为18
+     * @return studentAge传入的年龄
+     */
+    public int getStudentAge() {
+        return this.studentAge;
+    }
+
+    public void setStudentAge(int studentAge) {
+        if(studentAge<10||studentAge>100)
+            this.studentAge=18;
+        else
+            this.studentAge = studentAge;
+    }
+
+    /**
+     * 学生自我介绍的方法
+     * @return 自我介绍的信息，包括姓名、学号、性别、年龄
+     */
+    public String introduction(){
+        String str="学生信息如下：\n姓名："+getStudentName()+"\n学号："+getStudentNo()
+                +"\n性别："+getStudentGender() +"\n年龄："+getStudentAge();
+        return str;
+    }
+}
--- src/com/imooc/test/SchoolTest.java
+++ src/com/imooc/test/SchoolTest.java
@@ -1,4 +1,5 @@
 package com.imooc.test;
+import com.imooc.model.Student;
 import com.imooc.model.Subject;
 public class SchoolTest {
 
@@ -6,5 +7,7 @@ public class SchoolTest {
         Subject sub1 = new Subject("计算机科学与应用", "J001", 4);
         //如果年限输入为负数，则会返回int默认值
         System.out.println(sub1.info());
+        Student stu1=new Student("S01","张三","男",18);
+        System.out.println(stu1.introduction());
     }
 }
