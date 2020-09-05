1d6a00d104bab50054a6c336e000e417b2f5e461
Shiminmandy <mandyshimin18@gmail.com>
2020-09-04 22:00:37 -0400

    inheritate

---
+++ AnimalProj/src/com/imooc/animal/Dog.java
@@ -0,0 +1,21 @@
+package com.imooc.animal;
+
+public class Dog extends Animal{
+    private String Gender;//性别
+
+    private Dog(){
+
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public void sleep(){
+        System.out.println(this.getName()+"现在"+this.getMonth()+"个月大，它在睡觉");
+    }
+}
