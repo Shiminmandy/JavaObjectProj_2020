4cbeabd037a0247a79ab90159986e56ec79b20be
Shiminmandy <mandyshimin18@gmail.com>
2020-09-12 17:33:55 -0400

    inherit+super

--- AnimalProj/src/com/imooc/animal/Cat.java
+++ AnimalProj/src/com/imooc/animal/Cat.java
@@ -17,6 +17,14 @@ public class Cat extends Animal{
         System.out.println("我是子类的无参构造方法");
     }
 
+    public Cat(String name,int month){
+        /*子类构造默认调用父类无参方法
+         * 可以通过super()调用父类允许被访问的其他构造方法
+         * super()必须放在子类构造方法有效代码第一行
+         */
+        super(name,month);
+        System.out.println("我是子类的带参构造方法");
+    }
     public double getWeight() {
         return weight;
     }
