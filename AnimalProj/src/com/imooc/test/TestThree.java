85bf39899d3d308a05db0fbd3187bbd024275e1a
Shiminmandy <mandyshimin18@gmail.com>
2020-09-16 21:37:49 -0400

    equals

---
+++ AnimalProj/src/com/imooc/test/TestThree.java
@@ -0,0 +1,28 @@
+package com.imooc.test;
+
+import com.imooc.animal.Animal;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+//use of equals
+public class TestThree {
+    public static void main(String[] args){
+        Animal one=new Animal("花花",2);
+        Animal two=new Animal("花花",2);
+        //equals:继承Object中的equals方法是，比较的是两个引用是否指向同一个对象
+        boolean flag=one.equals(two);
+        System.out.println("one和two的引用比较："+flag);
+        System.out.println("one和two的引用比较："+(one==two));
+        System.out.println("=======================");
+        String str1=new String("hello");
+        String str2=new String("hello");
+        boolean flag1=str1.equals(str2);
+        System.out.println("str1和str2的引用比较："+flag);
+        System.out.println("str1和str2的引用比较："+(str1==str2));
+    }
+}
+/*
+one和two的引用比较：false
+one和two的引用比较：false
+=======================
+str1和str2的引用比较：false
+str1和str2的引用比较：false
+ */
