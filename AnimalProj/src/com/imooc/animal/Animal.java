d62bea5c4b26c5408f72fe6c78b88a274513a40c
Shiminmandy <mandyshimin18@gmail.com>
2020-09-19 22:10:54 -0400

    inherit+final

--- AnimalProj/src/com/imooc/animal/Animal.java
+++ AnimalProj/src/com/imooc/animal/Animal.java
@@ -1,5 +1,10 @@
 package com.imooc.animal;
 
+/*final class:该类没有子类 public final class\final public class
+ *final 方法：该方法不允许被子类重写，但是可以正常被子类继承使用
+ * final 方法内局部变量：只要在具体被使用之前进行赋值即可，一旦赋值不允许被修改
+ *       类中成员属性：赋值过程：1、定义直接初始化 2、构造方法 3、构造代码块
+ */
 public class Animal {
     /*private：只允许在本类中访问
      *public:允许在任意位置访问
