1e36767b5bddfb97ce7f3e244817414394c37b05
Shiminmandy <mandyshimin18@gmail.com>
2020-10-05 23:43:46 -0400

    Poly

---
+++ PolyProj/src/com/imooc/tel/Telephone.java
@@ -0,0 +1,34 @@
+package com.imooc.tel;
+
+/**
+ * 原始手机
+ */
+public class Telephone {
+    private String brand;
+    private int price;
+
+    public Telephone(){
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    //打电话
+    public void call(){
+        System.out.println("手机可以打电话");
+    }
+}
