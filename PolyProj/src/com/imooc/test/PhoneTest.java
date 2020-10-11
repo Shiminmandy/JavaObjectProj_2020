85c75fa8a140ef9b05f4ccf53901cf64d176af6d
Shiminmandy <mandyshimin18@gmail.com>
2020-10-10 22:24:19 -0400

    Poly

--- PolyProj/src/com/imooc/test/PhoneTest.java
+++ PolyProj/src/com/imooc/test/PhoneTest.java
@@ -1,5 +1,15 @@
 package com.imooc.test;
 
+import com.imooc.tel.ForthPhone;
+
 public class PhoneTest {
-    
+    public static void main(String[] args){
+        ForthPhone phone=new ForthPhone();
+        phone.call();
+        phone.message();
+        phone.vedio();
+        phone.music();
+        phone.photo();
+        phone.network();
+    }
 }
